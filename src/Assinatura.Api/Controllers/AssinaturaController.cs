19695f3ee3d205c6e17fee8eddd733405f0ff509
Rinaldo Serra <rinaldo.serra@tjse.jus.br>
2024-06-12 13:12:49 -0300

    feat: assinaturaController

---
+++ src/Assinatura.Api/Controllers/AssinaturaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assinatura.Api.Controllers
+{
+    [ApiController]
+    [Route("assinatura")]
+    public class AssinaturaController : ControllerBase
+    {
+        private readonly ILogger<AssinaturaController> _logger;
+        public AssinaturaController(ILogger<AssinaturaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IDictionary<int, string> Get()
+        {
+            IDictionary<int, string> list = new Dictionary<int, string>();
+            list.Add(1, "IPhone");
+            list.Add(2, "Laptop");
+            list.Add(3, "Samsung TV");
+            return list;
+        }
+    }
+}
